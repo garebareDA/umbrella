0d8549d12ebc5da5a9cee93f051ef7fd4a0193b7
garebare <garedaiki@outlook.jp>
2020-08-22 22:36:51 +0900

    add:test

--- src/parser/parsers.rs
+++ src/parser/parsers.rs
@@ -1,55 +1,64 @@
 extern crate lelex;
 
-use super::ast;
 use super::super::lexer::token;
+use super::ast;
 
 static TOKEN: token::Token = token::Token::new();
 
 pub struct Persers {
   tokens: Vec<lelex::tokens::Tokens>,
-  index:usize,
+  index: usize,
 }
 
-impl Persers{
+impl Persers {
   pub fn new(tokens: Vec<lelex::tokens::Tokens>) -> Self {
     Self {
       tokens: tokens,
-      index:0
+      index: 0,
     }
   }
 
-  pub fn run(&mut self) -> ast::RootAST{
+  pub fn run(&mut self) -> ast::RootAST {
     let mut root = ast::RootAST::new();
     let len = self.tokens.len();
-    loop{
+    loop {
       let result = self.judge();
 
-      match result{
+      match result {
         Ok(r) => {
           root.node.push(r);
         }
-
         Err(()) => {}
       }
 
       self.index += 1;
-      if len <= self.index{
+      if len <= self.index {
         break;
       }
     }
     return root;
   }
 
-  fn judge(&mut self)-> Result<ast::Types, ()> {
+  fn judge(&mut self) -> Result<ast::Types, ()> {
     let token = self.get_tokens(self.index).get_token();
+    let len = self.tokens.len();
 
     if token == TOKEN._variable {
-      if self.get_tokens(self.index + 1).get_token() == TOKEN._paren_left{
+      if len > 1 && self.get_tokens(self.index + 1).get_token() == TOKEN._paren_left {
         let value = self.get_tokens(self.index).get_value();
         let mut callee = ast::CallAST::new(value);
+
         self.index_add(2);
+
         let inner = self.judge();
-        callee.argument.push(inner.expect("error!"));
+        match inner {
+          Ok(inner) => {
+            callee.argument.push(inner);
+          }
+
+          Err(()) => {}
+        }
+
         self.index_add(1);
         return Ok(ast::Types::Call(callee));
       }
@@ -67,11 +76,50 @@ impl Persers{
     return Err(());
   }
 
-  fn get_tokens(&mut self, num: usize) -> &lelex::tokens::Tokens{
-    return &self.tokens[num]
+  fn get_tokens(&mut self, num: usize) -> &lelex::tokens::Tokens {
+    return &self.tokens[num];
   }
 
-  fn index_add(&mut self, num:usize) {
+  fn index_add(&mut self, num: usize) {
     self.index += num;
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::super::super::lexer::lexers;
+  use super::super::parsers;
+  use super::ast;
+  #[test]
+  fn variable() {
+    let lex_result = lexers::run("test");
+    let mut parser = parsers::Persers::new(lex_result);
+    let result = parser.run();
+    match result.node[0] {
+      ast::Types::Variable(_) => {}
+      _ => panic!("not"),
+    }
+  }
+
+  #[test]
+  fn strings() {
+    let lex_result = lexers::run("\"test\"");
+    let mut parser = parsers::Persers::new(lex_result);
+    let result = parser.run();
+    match result.node[0] {
+      ast::Types::Strings(_) => {}
+      _ => panic!("not"),
+    }
+  }
+
+  #[test]
+  fn callee() {
+    let lex_result = lexers::run("test()");
+    let mut parser = parsers::Persers::new(lex_result);
+    let result = parser.run();
+    match result.node[0] {
+      ast::Types::Call(_) => {}
+      _ => panic!("not"),
+    }
   }
 }
