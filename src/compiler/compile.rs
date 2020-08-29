ec617121e93e976aabc31a1b8ffbd00096efe728
garebare <garedaiki@outlook.jp>
2020-08-29 23:21:44 +0900

    wip

--- src/compiler/compile.rs
+++ src/compiler/compile.rs
@@ -84,6 +84,23 @@ impl<'ctx> CodeGen<'ctx> {
           _ => {}
         }
       }
+
+      ast::Types::Fors(fors) => {
+        let i32_type = self.context.i32_type();
+        let main_type = i32_type.fn_type(&[], false);
+        let function = self.module.add_function("fors", main_type, None);
+        let basic_block_entry = self.context.append_basic_block(function, "entry");
+        let basic_block_preloop = self.context.append_basic_block(function, "preloop");
+        let basic_block_loop = self.context.append_basic_block(function, "loop");
+        let basic_block_afterloop = self.context.append_basic_block(function, "afterloop");
+
+        self.builder.position_at_end(basic_block_entry);
+        self.builder.build_unconditional_branch(basic_block_preloop);
+        self.builder.position_at_end(basic_block_preloop);
+        let variable = self.builder.build_phi(i32_type, "i");
+        variable.add_incoming(&[(&i32_type.const_int(1, false), basic_block_entry)]);
+        
+      }
       _ => {}
     }
   }
