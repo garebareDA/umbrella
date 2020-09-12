9c356921f943760450f34e5f7d6648c418f3b45e
garebare <garedaiki@outlook.jp>
2020-09-12 16:02:09 +0900

    fix:calcuration

--- src/compiler/calcuration.rs
+++ src/compiler/calcuration.rs
@@ -67,136 +67,9 @@ impl<'ctx> CodeGen<'ctx> {
         return sum;
       }
     }
-
-    let mut cal_counter = 0;
-    for (i, op) in op_stack.iter().enumerate() {
-      if number_stack.len() == 1 {
-        break;
-      }
-      let l_index = i - cal_counter;
-      let r_index = i - cal_counter + 1;
-
-      let l_stack = number_stack[l_index].into_int_value();
-      let r_stack = number_stack[r_index].into_int_value();
-
-      if op == "/" {
-        let sum = self.builder.build_int_unsigned_div(l_stack, r_stack, "div");
-        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
-        number_stack.remove(r_index);
-      }
-
-      if op == "*" {
-        let sum = self.builder.build_int_mul(l_stack, r_stack, "mul");
-        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
-        number_stack.remove(r_index);
-      }
-
-      cal_counter += 1;
-    }
-
-    let mut cal_counter = 0;
-    for (i, op) in op_stack.iter().enumerate() {
-      if number_stack.len() == 1 {
-        break;
-      }
-      let l_index = i - cal_counter;
-      let r_index = i - cal_counter + 1;
-
-      let l_stack = number_stack[l_index].into_int_value();
-      let r_stack = number_stack[r_index].into_int_value();
-
-      if op == "+" {
-        let sum = self.builder.build_int_add(l_stack, r_stack, "sum");
-        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
-        number_stack.remove(r_index);
-      }
-
-      if op == "-" {
-        let sum = self.builder.build_int_sub(l_stack, r_stack, "sub");
-        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
-        number_stack.remove(r_index);
-      }
-
-      cal_counter += 1;
-    }
-
-    let mut cal_counter = 0;
-    for (i, op) in op_stack.iter().enumerate() {
-      if number_stack.len() == 1 {
-        break;
-      }
-      let l_index = i - cal_counter;
-      let r_index = i - cal_counter + 1;
-
-      let l_stack = number_stack[l_index].into_int_value();
-      let r_stack = number_stack[r_index].into_int_value();
-
-      if op == "<" {
-        let sum =
-          self
-            .builder
-            .build_int_compare(inkwell::IntPredicate::SLT, l_stack, r_stack, "lessthan");
-        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
-        number_stack.remove(r_index);
-      }
-
-      if op == ">" {
-        let sum = self.builder.build_int_compare(
-          inkwell::IntPredicate::SGT,
-          l_stack,
-          r_stack,
-          "greaterthan",
-        );
-        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
-        number_stack.remove(r_index);
-      }
-
-      if op == "<=" {
-        let sum = self.builder.build_int_compare(
-          inkwell::IntPredicate::SLE,
-          l_stack,
-          r_stack,
-          "greaterthanorequal",
-        );
-        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
-        number_stack.remove(r_index);
-      }
-
-      if op == ">=" {
-        let sum = self.builder.build_int_compare(
-          inkwell::IntPredicate::SGE,
-          l_stack,
-          r_stack,
-          "lessthanorequal",
-        );
-        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
-        number_stack.remove(r_index);
-      }
-
-      if op == "==" {
-        let sum = self.builder.build_int_compare(
-          inkwell::IntPredicate::EQ,
-          l_stack,
-          r_stack,
-          "equal",
-        );
-        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
-        number_stack.remove(r_index);
-      }
-
-      if op == "!=" {
-        let sum = self.builder.build_int_compare(
-          inkwell::IntPredicate::NE,
-          l_stack,
-          r_stack,
-          "notequal",
-        );
-        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
-        number_stack.remove(r_index);
-      }
-
-      cal_counter += 1;
-    }
+    self.calcuration_op(&op_stack, &mut number_stack, 1);
+    self.calcuration_op(&op_stack, &mut number_stack, 2);
+    self.calcuration_op(&op_stack, &mut number_stack, 3);
 
     return number_stack[0].into_int_value();
   }
@@ -235,6 +108,113 @@ impl<'ctx> CodeGen<'ctx> {
         Err(()) => {}
       },
       _ => {}
+    }
+  }
+
+  fn calcuration_op(
+    &self,
+    op_stack: &Vec<String>,
+    number_stack: &mut Vec<values::BasicValueEnum<'ctx>>,
+    ops: usize,
+  ) {
+    let before_len = number_stack.len();
+    for (i, op) in op_stack.iter().enumerate() {
+      let sub_len = before_len - number_stack.len();
+      let l_index = i - sub_len;
+      let r_index = i + 1 - sub_len;
+
+      if number_stack.len() == 1 || number_stack.len() <= r_index{
+        break;
+      }
+
+      let l_stack = number_stack[l_index].into_int_value();
+      let r_stack = number_stack[r_index].into_int_value();
+
+      if op == "*" && 1 == ops {
+        let sum = self.builder.build_int_mul(l_stack, r_stack, "mul");
+        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
+        number_stack.remove(r_index);
+      }
+
+      if op == "/" && 1 == ops {
+        println!("  {:?} {:?}", r_stack, l_stack);
+        let sum = self.builder.build_int_unsigned_div(l_stack, r_stack, "div");
+        println!("  {:?}", sum);
+        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
+        number_stack.remove(r_index);
+      }
+
+      if op == "+" && 2 == ops {
+        let sum = self.builder.build_int_add(l_stack, r_stack, "sum");
+        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
+        number_stack.remove(r_index);
+      }
+
+      if op == "-" && 2 == ops {
+        let sum = self.builder.build_int_sub(l_stack, r_stack, "sub");
+        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
+        number_stack.remove(r_index);
+      }
+
+      if op == "<" && 3 == ops {
+        let sum =
+          self
+            .builder
+            .build_int_compare(inkwell::IntPredicate::SLT, l_stack, r_stack, "lessthan");
+        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
+        number_stack.remove(r_index);
+      }
+
+      if op == ">" && 3 == ops {
+        let sum = self.builder.build_int_compare(
+          inkwell::IntPredicate::SGT,
+          l_stack,
+          r_stack,
+          "greaterthan",
+        );
+        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
+        number_stack.remove(r_index);
+      }
+
+      if op == "<=" && 3 == ops {
+        let sum = self.builder.build_int_compare(
+          inkwell::IntPredicate::SLE,
+          l_stack,
+          r_stack,
+          "greaterthanorequal",
+        );
+        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
+        number_stack.remove(r_index);
+      }
+
+      if op == ">=" && 3 == ops {
+        let sum = self.builder.build_int_compare(
+          inkwell::IntPredicate::SGE,
+          l_stack,
+          r_stack,
+          "lessthanorequal",
+        );
+        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
+        number_stack.remove(r_index);
+      }
+
+      if op == "==" && 3 == ops {
+        let sum =
+          self
+            .builder
+            .build_int_compare(inkwell::IntPredicate::EQ, l_stack, r_stack, "equal");
+        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
+        number_stack.remove(r_index);
+      }
+
+      if op == "!=" && 3 == ops {
+        let sum =
+          self
+            .builder
+            .build_int_compare(inkwell::IntPredicate::NE, l_stack, r_stack, "notequal");
+        number_stack[l_index] = values::BasicValueEnum::IntValue(sum);
+        number_stack.remove(r_index);
+      }
     }
   }
 }
