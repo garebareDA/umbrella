194607f29cf823db1a3c94c4c20eda03cea26f4f
garebare <garedaiki@outlook.jp>
2020-10-04 10:50:31 +0900

    add:args

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
 extern crate inkwell;
 
+use std::env;
 use std::fs;
 use umbrella::compiler::compile;
 use umbrella::lexer::lexers;
 use umbrella::parser::parsers;
 
 fn main() {
-    let content = fs::read_to_string("./example/example.um").unwrap();
-    let lex_result = lexers::run(&content);
-    let mut parser = parsers::Persers::new(lex_result);
-    let root = parser.run();
-    match root {
-        Ok(root) => {
-            println!("{:?}", root);
-            compile::compile(root);
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 4 && args[1] == "run" {
+        let content = fs::read_to_string(&args[2]).unwrap();
+        let lex_result = lexers::run(&content);
+        let mut parser = parsers::Persers::new(lex_result);
+        let root = parser.run();
+        match root {
+            Ok(root) => {
+                println!("{:?}", root);
+                compile::compile(root, &args[3]);
+            }
+            Err(s) => {
+                eprintln!("{}", s);
+            }
         }
-        Err(s) => {
-            eprintln!("{}", s);
-        }
+    }else {
+        println!("run [input path] [output path]");
     }
 }
